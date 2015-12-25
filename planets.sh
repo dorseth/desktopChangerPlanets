47496ab01749b6b38d2bd30d57ac4fc9302064eb
dorseth <josh.rubyist@gmail.com>
2015-12-26 01:52:09 +0200

    First commit

---
+++ planets.sh
@@ -0,0 +1,32 @@
+ 
+
+
+if [ $(date +%u) -eq 1 ]; then
+ gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/dorsett/Pictures/planets/mon.jpg"
+fi
+
+if [ $(date +%u) -eq 2 ]; then
+  gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/dorsett/Pictures/planets/tue.jpg"
+
+fi
+
+if [ $(date +%u) -eq 3 ]; then
+ 	 gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/dorsett/Pictures/planets/wed.jpg"
+fi
+
+if [ $(date +%u) -eq 4 ]; then
+	gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/dorsett/Pictures/planets/thu.jpg"
+fi
+
+if [ $(date +%u) -eq 5 ]; then
+ gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/dorsett/Pictures/planets/fri.jpg"
+fi
+
+if [ $(date +%u) -eq 6 ]; then
+  gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/dorsett/Pictures/planets/sat.jpg"
+fi
+
+if [ $(date +%u) -eq 7 ]; then
+ gsettings set org.cinnamon.desktop.background picture-uri  "file:///home/dorsett/Pictures/planets/sun.jpg"
+fi
+
